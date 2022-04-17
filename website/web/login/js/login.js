d98cb0f34ffc916beeaa658d8af1a1d069602e01
Ekaterina Borisova <kate_boriso2002@mail.ru>
2022-04-17 19:08:13 +0300

    Fix test login example

---
+++ website/web/login/js/login.js
@@ -0,0 +1,21 @@
+function login() {
+
+    var user = document.getElementById('email').value;
+    alert(user);
+    var pass = document.getElementById('pass').value;
+    alert(pass);
+    var req = new XMLHttpRequest();
+
+    req.open("POST", "http://localhost:8080/archery/auth/signIn");
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.withCredentials = true;
+    req.send(JSON.stringify({login: user, password: pass}));
+    req.onload = function () {
+        if (req.status !== 200) {
+            alert("Fail");
+        } else {
+            alert("Success");
+            location.href = "../../lk.html";
+        }
+    }
+}
