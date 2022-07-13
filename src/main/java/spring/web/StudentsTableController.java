8e5076369937e8cad2eca5587c172e93c046bf56
Marta <ezerinya.m@edu.spbstu.ru>
2022-07-13 22:02:28 +0300

    change methods to work with authorization

--- src/main/java/spring/web/StudentsTableController.java
+++ src/main/java/spring/web/StudentsTableController.java
@@ -3,12 +3,14 @@ package spring.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import spring.entity.PurchaseHistory;
 import spring.entity.Rank;
 import spring.entity.SeasonTicket;
 import spring.entity.Student;
 import spring.exception.PurchaseNotFoundException;
+import spring.exception.StudentNotFoundException;
 import spring.repositories.SeasonTicketRepository;
 import spring.repositories.StudentRepository;
 import spring.requests.StudentAndRankRequest;
@@ -24,8 +26,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/archery/admin/students")
 @RequiredArgsConstructor
-public class StudentsTableController
-{
+public class StudentsTableController {
     private final StudentService studentService;
     private final StudentRepository studentRepository;
     private final SeasonTicketRepository ticketRepository;
@@ -33,49 +34,68 @@ public class StudentsTableController
     private final RankService rankService;
 
     @GetMapping("")
-    public ResponseEntity<List<Student>> showStudents()
-    {
+    public ResponseEntity<List<Student>> showStudents() {
         return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping("/id")
-    public ResponseEntity<Student> showStudent(@RequestBody Long id)
-    {
-        return new ResponseEntity<>(studentService.findStudentById(id), HttpStatus.OK);
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Student> showStudent(@PathVariable Long id, Authentication auth) {
+        if (auth == null) { // не должно никогда срабатывать, тк отлавливается в JwtFilter
+            //значит время сеанса истекло и тут перенаправлять на страницу с логином
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); //401
+        }
+        try {
+            return new ResponseEntity<>(studentService.findStudentById(id), HttpStatus.OK);
+        } catch (StudentNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); //400
+        }
     }
 
-    @GetMapping("/ticket")
-    public ResponseEntity<TicketInfoRequest> showTicket(@RequestBody Long id)
-    {
-        String ticket = "";
-        TicketInfoRequest ticketInfoRequest = new TicketInfoRequest("", 0);
-        try
-        {
-            PurchaseHistory purchaseHistory = purchaseHistoryService.findPurchaseWithActiveSeasonTicket(id, LocalDate.now());
-            ticketInfoRequest.setTicketType(purchaseHistory.getSeasonTicket().getTicketType());
-            ticketInfoRequest.setAvailableClasses(purchaseHistory.getAvailableClasses());
+    @GetMapping("/ticket/{id}")
+    public ResponseEntity<TicketInfoRequest> showTicket(@PathVariable Long id, Authentication auth) {
+        if (auth == null) { // не должно никогда срабатывать, тк отлавливается в JwtFilter
+            //значит время сеанса истекло и тут перенаправлять на страницу с логином
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); //401
         }
-        catch (PurchaseNotFoundException ignored) {}
-        return new ResponseEntity<>(ticketInfoRequest, HttpStatus.OK);
+        try {
+            String ticket = "";
+            TicketInfoRequest ticketInfoRequest = new TicketInfoRequest("", 0);
+            try {
+                PurchaseHistory purchaseHistory = purchaseHistoryService.findPurchaseWithActiveSeasonTicket(id, LocalDate.now());
+                ticketInfoRequest.setTicketType(purchaseHistory.getSeasonTicket().getTicketType());
+                ticketInfoRequest.setAvailableClasses(purchaseHistory.getAvailableClasses());
+            } catch (PurchaseNotFoundException ignored) {
+            }
+            return new ResponseEntity<>(ticketInfoRequest, HttpStatus.OK);
+        } catch (StudentNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); //400
+        }
     }
 
     @GetMapping("/tickets")
-    public ResponseEntity<List<String>> findTickets()
-    {
+    public ResponseEntity<List<String>> findTickets() {
         return new ResponseEntity<>(ticketRepository.findAll().stream().map(SeasonTicket::getTicketType).toList(), HttpStatus.OK);
     }
 
     @PostMapping("/changeticket")
-    public void editTicket(@RequestBody StudentAndTicketRequest studentAndTicketRequest)
-    {
-        Student student = studentService.findStudentById(studentAndTicketRequest.getId());
-        SeasonTicket seasonTicket = ticketRepository.findByTicketType(studentAndTicketRequest.getSeasonTicketType()).get();
-        purchaseHistoryService.addPurchase(student, LocalDate.now(), seasonTicket);
+    public ResponseEntity<HttpStatus> editTicket(@RequestBody StudentAndTicketRequest studentAndTicketRequest, Authentication auth) {
+        if (auth == null) { // не должно никогда срабатывать, тк отлавливается в JwtFilter
+            //значит время сеанса истекло и тут перенаправлять на страницу с логином
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED); //401
+        }
+        try {
+            Student student = studentService.findStudentById(studentAndTicketRequest.getId());
+            SeasonTicket seasonTicket = ticketRepository.findByTicketType(studentAndTicketRequest.getSeasonTicketType()).get();
+            purchaseHistoryService.addPurchase(student, LocalDate.now(), seasonTicket);
+            System.out.println("SeasonTicket is updated");
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (StudentNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); //400
+        }
     }
 
     @GetMapping("/ranks")
-    public ResponseEntity<List<String>> findRanks()
-    {
+    public ResponseEntity<List<String>> findRanks() {
         return new ResponseEntity<>(rankService.findAll().stream().map(Rank::getRank_name).toList(), HttpStatus.OK);
     }
 
